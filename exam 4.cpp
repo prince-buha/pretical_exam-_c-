1d46e44dfe3b66dc8c4a1d13167e80d46b9bf402
prince-buha <150029430+prince-buha@users.noreply.github.com>
2024-02-02 18:15:22 +0530

    first commit

--- exam 4.cpp
+++ exam 4.cpp
@@ -1,7 +1,44 @@
+/*
+4. Write a Program which illustrates Method Overriding by using three classes.
+
+the number of a:-2
+the number of b:-2
+the sum of a and b is2
+*/
 #include<iostream>
 using namespace std;
 
 class A{
 	public :
-		
+		void illustrates(){
+			int a;
+			cout<<"the number of a:-";
+			cin>>a;
+		}
 };
+class B {
+	public :
+		void illustrates(){
+			int b;
+			cout<<"the number of b:-";
+			cin>>b;
+		}
+};
+class C : public A,public B{
+	public :
+		void illustrates(){
+			int c;
+			cout<<"the sum of a and b is :"<<c;
+			
+		}
+};
+int main(){
+	C c1;
+	
+	c1.A::illustrates();
+	c1.B::illustrates();
+	c1.C::illustrates();
+	
+	
+	return 0;
+}
